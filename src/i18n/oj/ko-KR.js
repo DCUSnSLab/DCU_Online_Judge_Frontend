2cb2c251525c9c22acf5f2f4a35bb65fb5729897
Munjiwon <wldnjsdl269@gmail.com>
2024-02-23 18:18:56 +0900

    modify

--- src/i18n/oj/ko-KR.js
+++ src/i18n/oj/ko-KR.js
@@ -192,7 +192,7 @@ export const m = {
   List_Solved_Problems: '푼 문제들',
   UserHomeIntro: '아직 어떠한 문제도 풀지 않았습니다.',
   Test: '테스트',
-  Regenerate: 'regenerate',
+  Regenerate: '갱신',
   // Lecture
   Signup_Lectures: '수강 과목',
   Lectures: '개설 과목',
