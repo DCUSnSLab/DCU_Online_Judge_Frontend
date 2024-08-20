1847f1444051b27ea81f5808ca1c576c016b9161
backgroundmin <49299170+backgroundmin@users.noreply.github.com>
2024-08-20 02:09:58 +0000

    add name

--- src/pages/oj/views/terminal/Terminal.vue
+++ src/pages/oj/views/terminal/Terminal.vue
@@ -1,21 +1,24 @@
 <template>
   <panel>
-    <div class="content markdown-body">
-      <iframe
-        id="iframeExample"
-        width="100%"
-        height="800px"
-        src="http://localhost:80"
-        frameborder="0"
-        allowfullscreen
-      ></iframe>
-    </div>
+    <iframe
+      id="webssh"
+      name="webssh"
+      width="100%"
+      height="800px"
+      src="http://localhost:2222/ssh/host/127.0.0.1"
+      frameborder="0"
+      allowfullscreen
+    ></iframe>
   </panel>
 </template>
 
 <script>
 export default {
-  name: 'Terminal'
+  name: 'Terminal',
+  methods: {
+    submitForm () {
+    }
+  }
 }
 </script>
 
