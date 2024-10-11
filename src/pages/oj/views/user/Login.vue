9c683e3a22785c69cc7a05820f5b37fe4baec81f
backgroundmin <49299170+backgroundmin@users.noreply.github.com>
2024-10-11 08:38:27 +0000

    Login get token

--- src/pages/oj/views/user/Login.vue
+++ src/pages/oj/views/user/Login.vue
@@ -100,9 +100,8 @@
             delete formData['tfa_code']
           }
           api.login(formData).then(res => {
-            console.log(res)
-            localStorage.setItem('access_token', res.data.tokens.access)
-            localStorage.setItem('refresh_token', res.data.tokens.refresh)
+            localStorage.setItem('access_token', res.data.data.tokens.access)
+            localStorage.setItem('refresh_token', res.data.data.tokens.refresh)
             this.btnLoginLoading = false
             this.changeModalStatus({visible: false})
             this.getProfile()
