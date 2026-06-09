338a2c788d36a396ef68ee1c5e006e2ef4152171
backgroundmin <zxc81808080@gmail.com>
2026-06-09 22:06:45 +0900

    feat(GEN-1045): 계정 설정 SSO account 리다이렉트 (비번/이메일 폼 제거)

--- src/pages/oj/views/setting/children/AccountSetting.vue
+++ src/pages/oj/views/setting/children/AccountSetting.vue
@@ -1,194 +1,45 @@
 <template>
   <div class="setting-main">
-    <div class="flex-container">
-      <div class="left">
-        <p class="section-title">{{$t('m.ChangePassword')}}</p>
-        <Form class="setting-content" ref="formPassword" :model="formPassword" :rules="rulePassword">
-          <FormItem :label="$t('m.Password_use')" prop="old_password">
-            <Input v-model="formPassword.old_password" type="password"/>
-          </FormItem>
-          <FormItem :label="$t('m.New_password')" prop="new_password">
-            <Input v-model="formPassword.new_password" type="password"/>
-          </FormItem>
-          <FormItem :label="$t('m.New_password_Again')" prop="again_password">
-            <Input v-model="formPassword.again_password" type="password"/>
-          </FormItem>
-          <FormItem v-if="visible.tfaRequired" label="Two Factor Auth" prop="tfa_code">
-            <Input v-model="formPassword.tfa_code"/>
-          </FormItem>
-          <FormItem v-if="visible.passwordAlert">
-            <Alert type="success">{{$t('m.Password_Change_ment') }}</Alert>
-          </FormItem>
-          <Button type="primary" @click="changePassword">{{$t('m.Update_Password')}}</Button>
-        </Form>
-      </div>
-
-      <div class="middle separator"></div>
-
-      <div class="right">
-        <p class="section-title">{{$t('m.ChangeEmail')}}</p>
-        <Form class="setting-content" ref="formEmail" :model="formEmail" :rules="ruleEmail">
-          <FormItem :label="$t('m.Password_use')" prop="password">
-            <Input v-model="formEmail.password" type="password"/>
-          </FormItem>
-          <FormItem :label="$t('m.Email_use')">
-            <Input v-model="formEmail.old_email" disabled/>
-          </FormItem>
-          <FormItem :label="$t('m.Email_change')" prop="new_email">
-            <Input v-model="formEmail.new_email"/>
-          </FormItem>
-          <FormItem v-if="visible.tfaRequired" :label="$t('m.Two_Factor_Authentication')" prop="tfa_code">
-            <Input v-model="formEmail.tfa_code"/>
-          </FormItem>
-          <Button type="primary" @click="changeEmail">{{$t('m.ChangeEmail')}}</Button>
-        </Form>
-      </div>
+    <p class="section-title">{{$t('m.Account')}}</p>
+    <div class="sso-redirect">
+      <p class="desc">
+        비밀번호 · 이메일 · 2단계 인증 등 계정 보안 설정은 통합 계정(DCUCODE SSO)에서 관리합니다.
+      </p>
+      <p class="moving">SSO 계정 관리 페이지로 이동합니다…</p>
+      <Button type="primary" @click="goSso">SSO 계정 관리로 이동</Button>
     </div>
   </div>
 </template>
 
 <script>
-  import api from '@oj/api'
-  import { FormMixin } from '@oj/components/mixins'
-  import JSEncrypt from 'jsencrypt'
-
+  // dcu-sso 전환: 계정(비번/이메일/2FA) 관리는 SSO /account 로 일원화.
+  // 진입 즉시 backend(/api/auth/oidc/account)로 이동 → SSO 가 SSO_ISSUER/account 로 302.
+  // (기존 비밀번호 변경 · 이메일 변경 폼은 제거됨 — SSO 에서 처리)
   export default {
-    mixins: [FormMixin],
-    data () {
-      const oldPasswordCheck = [{required: true, trigger: 'blur', min: 6, max: 20}]
-      const tfaCheck = [{required: true, trigger: 'change'}]
-      const CheckAgainPassword = (rule, value, callback) => {
-        if (value !== this.formPassword.new_password) {
-          callback(new Error(this.$i18n.t('m.password_does_not_match')))
-        }
-        callback()
-      }
-      const CheckNewPassword = (rule, value, callback) => {
-        if (this.formPassword.old_password !== '') {
-          if (this.formPassword.old_password === this.formPassword.new_password) {
-            callback(new Error(this.$i18n.t('m.New_password_not_change')))
-          } else {
-            // 对第二个密码框再次验证
-            this.$refs.formPassword.validateField('again_password')
-          }
-        }
-        callback()
-      }
-      return {
-        loading: {
-          btnPassword: false,
-          btnEmail: false
-        },
-        visible: {
-          passwordAlert: false,
-          emailAlert: false,
-          tfaRequired: false
-        },
-        formPassword: {
-          tfa_code: '',
-          old_password: '',
-          new_password: '',
-          again_password: ''
-        },
-        formEmail: {
-          tfa_code: '',
-          password: '',
-          old_email: '',
-          new_email: ''
-        },
-        rulePassword: {
-          old_password: oldPasswordCheck,
-          new_password: [
-            {required: true, trigger: 'blur', min: 6, max: 20},
-            {validator: CheckNewPassword, trigger: 'blur'}
-          ],
-          again_password: [
-            {required: true, validator: CheckAgainPassword, trigger: 'change'}
-          ],
-          tfa_code: tfaCheck
-        },
-        ruleEmail: {
-          password: oldPasswordCheck,
-          new_email: [{required: true, type: 'email', trigger: 'change'}],
-          tfa_code: tfaCheck
-        }
-      }
-    },
-    mounted () {
-      this.formEmail.old_email = this.$store.getters.user.email || ''
+    created () {
+      this.goSso()
     },
     methods: {
-      async changePassword () {
-        await api.getPublicKey().then(res => {
-          this.public_key = res.data.data.public_key
-        })
-        this.validateForm('formPassword').then(valid => {
-          this.loading.btnPassword = true
-          const encrypt = new JSEncrypt()
-          encrypt.setPublicKey(this.public_key)
-          // this.formPassword.new_password = encrypt.encrypt(this.formPassword.new_password)
-          let data = Object.assign({}, this.formPassword)
-          data.new_password = encrypt.encrypt(this.formPassword.new_password)
-          console.log(data)
-          delete data.again_password
-          if (!this.visible.tfaRequired) {
-            delete data.tfa_code
-          }
-          api.changePassword(data).then(res => {
-            this.loading.btnPassword = false
-            this.visible.passwordAlert = true
-            this.$success(this.$i18n.t('m.Success_password_change'))
-            setTimeout(() => {
-              this.visible.passwordAlert = false
-              this.$router.push({name: 'logout'})
-            }, 5000)
-          }, res => {
-            if (res.data.data === 'tfa_required') {
-              this.visible.tfaRequired = true
-            }
-            this.loading.btnPassword = false
-          })
-        })
-      },
-      changeEmail () {
-        this.validateForm('formEmail').then(valid => {
-          this.loading.btnEmail = true
-          let data = Object.assign({}, this.formEmail)
-          if (!this.visible.tfaRequired) {
-            delete data.tfa_code
-          }
-          api.changeEmail(data).then(res => {
-            this.loading.btnEmail = false
-            this.visible.emailAlert = true
-            this.$success(this.$i18n.t('m.Success_email_change'))
-            this.$refs.formEmail.resetFields()
-          }, res => {
-            if (res.data.data === 'tfa_required') {
-              this.visible.tfaRequired = true
-            }
-          })
-        })
+      goSso () {
+        window.location.href = '/api/auth/oidc/account'
       }
     }
   }
 </script>
 
 <style lang="less" scoped>
-
-  .flex-container {
-    justify-content: flex-start;
-    .left {
-      flex: 1 0;
-      width: 250px;
-      padding-right: 5%;
+  .sso-redirect {
+    padding: 10px 4px;
+    .desc {
+      font-size: 14px;
+      color: #57606a;
+      line-height: 1.6;
+      margin-bottom: 14px;
     }
-    > .middle {
-      flex: none;
-    }
-    .right {
-      flex: 1 0;
-      width: 250px;
+    .moving {
+      font-size: 13px;
+      color: #8a929e;
+      margin-bottom: 18px;
     }
   }
 </style>
-
